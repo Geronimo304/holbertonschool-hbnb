5b6987747bbbc69e5b3806d17dbc755637fa8e19
Agustin800 <aguslahalo@gmail.com>
2025-07-10 23:37:55 +0000

    put de user con jwt

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -1,5 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Namespace('users', description='User operations')
 
@@ -57,11 +58,18 @@ class UserResource(Resource):
     @api.expect(user_model, validate=True)
     @api.response(200, 'User successfully updated')
     @api.response(404, 'User not found')
+    @jwt_required()
     def put(self, user_id):
         """Update user details"""
+        current_user = get_jwt_identity()
+        current_user_id = current_user["id"]
         user = facade.get_user(user_id)
+
         if not user:
             return {'error': 'User not found'}, 404
+
+        if user_id != current_user_id:
+            return {'error': 'Unauthorized action'}
 
         user_data = api.payload
         try:
