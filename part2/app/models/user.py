ebf97588651d4be05a55e9495a0e0c256c219132
sofia1098 <sofianunez000@gmail.com>
2025-10-18 21:56:38 -0300

    class user

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,36 +1,27 @@
-from datetime import datetime
 import uuid
+from datetime import datetime
+from user import User
+"""Modulo Place"""
 
-class User:
-    """Representa un user"""
 
-    def __init__(self, first_name: str, last_name: str, email: str, is_admin: bool = False):
-        self.id = str(uuid.uuid4())  # identificador unico
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
-        self.validate()
-
-        # Validar datos básicos
-        self.validate()
-
-    def validate(self):
-        if not self.first_name or len(self.first_name) > 50:
-            raise ValueError("El nombre es obligatorio y debe tener menos de 50 caracteres.")
-
-        if not self.last_name or len(self.last_name) > 50:
-            raise ValueError("El apellido es obligatorio y debe tener menos de 50 caracteres.")
-
-        if not self.email or "@" not in self.email:
-            raise ValueError("El email es obligatorio y debe tener un formato válido.")
+class Place:
+    def __init__(self, title: str, description: str, price: float, latitude: float, longitude: float, owner: User):
+        self.id = str(uuid.uuid4())
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def update(self, **kwargs):
+        """Actualiza atributos del lugar"""
         for key, value in kwargs.items():
             if hasattr(self, key):
                 setattr(self, key, value)
-        self.updated_at = "Usuario actualizado"
-        self.validate()
+        self.updated_at = datetime.utcnow()
 
     def __repr__(self):
-        return f"<User {self.first_name} {self.last_name} ({self.email})>"
+        return f"<Place {self.title} - Ubicación: {self.latitude}, {self.longitude}>"
