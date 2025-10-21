cba6d3d029b5ebb20acb5c9c61920505bbb0677e
sofia1098 <sofianunez000@gmail.com>
2025-10-21 00:10:16 +0000

    more endpoints

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -13,10 +13,44 @@ class Place(BaseModel):
         self.latitude = latitude
         self.longitude = longitude
         self.owner = owner
+
+        # Relaciones en memoria no esoy segura si esto va
         self.reviews = []     # lista de reviews asociadas
         self.amenities = []   # lista de amenities asociadas
         owner.add_place(self)  # agrega este place al usuario automáticamente
    
+    # valiadaciones automaticas
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (int, float)) or value < 0:
+            raise ValueError("Price must be a non-negative number.")
+        self._price = float(value)
+
+    @property
+    def latitude(self):
+        return self._latitude
+
+    @latitude.setter
+    def latitude(self, value):
+        if not isinstance(value, (int, float)) or not -90 <= value <= 90:
+            raise ValueError("Latitude must be between -90 and 90.")
+        self._latitude = float(value)
+
+    @property
+    def longitude(self):
+        return self._longitude
+
+    @longitude.setter
+    def longitude(self, value):
+        if not isinstance(value, (int, float)) or not -180 <= value <= 180:
+            raise ValueError("Longitude must be between -180 and 180.")
+        self._longitude = float(value)
+
+    
     def update(self, **kwargs):
         for key, value in kwargs.items():
             if hasattr(self, key):
