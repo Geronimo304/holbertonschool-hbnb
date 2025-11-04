e6bcc3038690dfedeef9d069d6e4a8924d2dd1b6
HB10065 <10065@holbertonstudens.com>
2025-11-04 06:54:41 -0800

    facade metodos de amenity

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,8 +1,11 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
 
     def create_user(self, user_data):
         user = User(**user_data)
@@ -16,20 +19,21 @@ class HBnBFacade:
         return self.user_repo.get_by_attribute('email', email)
 
     def create_amenity(self, amenity_data):
-        # Placeholder for logic to create an amenity
-        pass
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
 
     def get_amenity(self, amenity_id):
-        # Placeholder for logic to retrieve an amenity by ID
-        pass
+        return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-        # Placeholder for logic to retrieve all amenities
-        pass
+        return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
-        # Placeholder for logic to update an amenity
-        pass
+        amenity = self.get_amenity(amenity_id)
+        amenity.edit_amenity(amenity_data)
+        self.amenity_repo.update(amenity_id, amenity)
+
     def create_place(self, place_data):
         # Placeholder for logic to create a place, including validation for price, latitude, and longitude
         pass
