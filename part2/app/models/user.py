5803906bf225395aad9ad1fdc8976066716ab2e9
sofia1098 <sofianunez000@gmail.com>
2025-11-05 13:51:54 +0000

    Correcion en models

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -8,9 +8,9 @@ class User(BaseModel):
 
     def __init__(self, first_name: str, last_name: str, email: str, is_admin: bool = False):
         super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
+        self._first_name = first_name
+        self._last_name = last_name
+        self._email = email
         self.is_admin = is_admin
 
     @property
