288bd2623ebe8abbb5ea9c0c38480befedc2c257
sofia1098 <sofianunez000@gmail.com>
2025-11-05 14:49:43 +0000

    Namespaces and config flask

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
 
-def create_app():
+db = SQLAlchemy()
+
+db.init_app(app)
+
+
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
+    app.config.from_object(config_class)
+    # ...
+
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
-    #api.add_namespace(places_ns)
-    #api.add_namespace(reviews_ns)
-    #api.add_namespace(amenities_ns)
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     return app
