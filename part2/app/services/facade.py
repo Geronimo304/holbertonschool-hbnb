dec53e6b7be1bdbba5c6cf88f6a8ac786cbcb8a7
HB10065 <10065@holbertonstudens.com>
2025-11-05 05:37:29 -0800

    added imports for place and review  and created review_repo

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,12 +1,15 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.amenity import Amenity
+from app.models.place import Place
+from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
 
     def create_user(self, user_data):
         user = User(**user_data)
