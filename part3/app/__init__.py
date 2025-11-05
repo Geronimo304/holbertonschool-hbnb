ac8508fadbe2157235ca5b7d15bae895d3289d7d
HB10065 <10065@holbertonstudens.com>
2025-11-05 09:32:42 -0800

    import and initialization of JWT

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 
+jwt = JWTManager()
 bcrypt = Bcrypt()
 db = SQLAlchemy()
 
@@ -12,6 +14,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config.from_object(config_class)
     bcrypt.init_app(app)
     db.init_app(app)
+    jwt.init_app(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
 
 
