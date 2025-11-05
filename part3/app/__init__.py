e06d5c49684acf0e54cffcb13a6b1a4ac9b637ef
HB10065 <10065@holbertonstudens.com>
2025-11-05 07:32:40 -0800

    fixed some imports in the facade and on __init__ from app, also changed db.init_app to inside create_app

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,19 +2,21 @@ from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.amenities import api as amenities_ns
 from flask_bcrypt import Bcrypt
 
+
 bcrypt = Bcrypt()
-
 db = SQLAlchemy()
-
-db.init_app(app)
-
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    db.init_app(app)
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
 
 
     # Register the users namespace
