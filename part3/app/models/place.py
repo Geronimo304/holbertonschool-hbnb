fd9b751325ba31f956b65ee80445eb04c819df30
sofia1098 <sofianunez000@gmail.com>
2025-11-12 15:50:28 +0000

    a

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -3,11 +3,11 @@ from app.models.user import User
 from app import db
 
 
-# place_amenity = db.Table(
-#     'place_amenity',
-#     db.Column('place_id', db.String(36), db.ForeignKey('places.id'), primary_key=True),
-#     db.Column('amenity_id', db.String(36), db.ForeignKey('amenities.id'), primary_key=True)
-# )
+ place_amenity = db.Table(
+     'place_amenity',
+     db.Column('place_id', db.String(36), db.ForeignKey('places.id'), primary_key=True),
+     db.Column('amenity_id', db.String(36), db.ForeignKey('amenities.id'), primary_key=True)
+ )
 
 class Place(BaseModel):
     """Place entity class."""
