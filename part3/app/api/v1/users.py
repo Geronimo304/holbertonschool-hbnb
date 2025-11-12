0275279a317742c2c9e13e7da091a2a9a603021a
sofia1098 <sofianunez000@gmail.com>
2025-11-12 14:43:06 +0000

    delete admi.py

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -50,3 +50,63 @@ class UserResource(Resource):
             'last_name': user.last_name,
             'email': user.email
             }, 200
+
+
+@api.route('/<user_id>')
+class AdminUserModify(Resource):
+    @api.response(403, 'Unauthorized action')
+    @api.response(400, 'Email already in use')
+    @api.response(404, 'User not found')
+    @api.response(200, 'User updated successfully')
+    @jwt_required()
+    def put(self, user_id):
+        # Administrador modifica detalles del usuario
+        current_user = get_jwt()
+        if not current_user.get('is_admin'):
+            return {'error': 'Admin privileges required'}, 403
+
+        data = api.payload or {}
+        email = data.get('email')
+
+        if email:
+            existing_user = facade.get_user_by_email(email)
+            if existing_user and existing_user.id != user_id:
+                return {'error': 'Email already in use'}, 400
+        
+        updated_user = facade.update_user(user_id, data)
+        if not updated_user:
+            return {'error': 'User not found'}, 404
+
+        return {'message': 'User updated successfully'}, 200
+
+
+
+@api.route('/')
+class AdminUserCreate(Resource):
+    @api.expect(user_model, validate=True)
+    @api.response(403, 'Admin privileges required')
+    @api.response(400,'Email already registered')
+    @api.response(201, 'User created successfully by admin')
+    @jwt_required()
+    def post(self):
+        # Administrador crea usuario
+        try: 
+            current_user = get_jwt()
+            if not current_user.get('is_admin'):
+                return {'error': 'Admin privileges required'}, 403
+
+            user_data = api.payload or {}
+            email = user_data.get('email')
+
+            if email:
+            existing_user = facade.get_user_by_email(email)
+            if existing_user and existing_user.id != user_id:
+                return {'error': 'Email already in use'}, 400
+
+            new_user = facade.create_user(user_data)
+            return {
+                'id': new_user.id,
+                'message': 'User created successfully by admin'
+            }, 201
+        except Exception as e:
+            return {'error': str(e)}, 400
